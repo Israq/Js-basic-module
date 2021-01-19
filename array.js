85bf359d1a0b90b3a5027ae28af4d3046d562050
Syed Ragib Israq <35467653+Israq@users.noreply.github.com>
2021-01-19 23:12:17 +0600

    first

---
+++ array.js
@@ -0,0 +1,5 @@
+friendsAge = [12,145,170,12121,2222323,112342123,123213];
+someAge = friendsAge[3];
+friendsAge.shift(13);
+console.log(friendsAge);
+
---
+++ conditionals.js
@@ -0,0 +1,13 @@
+var jobPaiso = false;
+var savings = 5000;
+if(jobPaiso==true && savings>100000){
+    console.log("Tomar jnno patri khuji")
+
+}
+else if(jobPaiso==true){
+    console.log("Kichu din por")
+
+}
+else{
+    console.log("patri nai")
+}
