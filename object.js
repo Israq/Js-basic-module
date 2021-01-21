6df74db9c9a399ad6747e720eeffab08123451aa
Syed Ragib Israq <35467653+Israq@users.noreply.github.com>
2021-01-21 20:40:28 +0600

    first

---
+++ object.js
@@ -0,0 +1,6 @@
+var student1=  {id:13213,name:"something",phone:1221312};
+var student2=  {id:13213,name:"something2",phone:12213121231231};
+
+console.log(student1);
+id2 = student2.id;
+console.log(id2)
